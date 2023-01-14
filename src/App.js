40dfca0e0a0c53ee99ed26a1dfdd1fa07a777f3b
Jack Wuersch <43575552+jackwuersch@users.noreply.github.com>
2023-01-14 23:16:25 +0900

    some styling changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ const App = () =>  {
   const categories = [
     {
       "id": 1,
-      "title": "boards",
+      "title": "BOARDS",
       "imageUrl": "https://i1.adis.ws/i/ride/ride_2223_feature-banner-sm_thrash-collection-lp?w=408&sm=aspect&aspect=408:302&scaleFit=poi&poi={$this.metadata.pointOfInterest.x},{$this.metadata.pointOfInterest.y},{$this.metadata.pointOfInterest.w},{$this.metadata.pointOfInterest.h}&qlt=75&fmt=webp&fmt.interlaced=true&dpi=96"
     },
     {
       "id": 2,
-      "title": "bindings",
+      "title": "BINDINGS",
       "imageUrl": "https://i1.adis.ws/i/ride/ride_2223_mm-banner_bindings_composite-series?w=342&sm=aspect&aspect=342:229&scaleFit=poi&poi={$this.metadata.pointOfInterest.x},{$this.metadata.pointOfInterest.y},{$this.metadata.pointOfInterest.w},{$this.metadata.pointOfInterest.h}&qlt=75&fmt=webp&fmt.interlaced=true&dpi=96"
     },
     {
       "id": 3,
-      "title": "boots",
+      "title": "BOOTS",
       "imageUrl": "https://i1.adis.ws/i/ride/ride_2223_mm-banner_boots_lasso-pro?w=342&sm=aspect&aspect=342:229&scaleFit=poi&poi={$this.metadata.pointOfInterest.x},{$this.metadata.pointOfInterest.y},{$this.metadata.pointOfInterest.w},{$this.metadata.pointOfInterest.h}&qlt=75&fmt=webp&fmt.interlaced=true&dpi=96"
     }
   ]
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -9,7 +9,6 @@ const CategoryItem = ({ category }) => {
             }} />
             <div className="category-body-container">
             <h2>{title}</h2>
-            <p>Shop now</p>
             </div>
         </div>
     )
