8203587e093bade4cfb65548ede9bd642284d038
Jack Wuersch <43575552+jackwuersch@users.noreply.github.com>
2023-01-17 19:16:42 +0900

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './components/pages/home/home.component';
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <h1>Header</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return (
+    <div>
+        <h1>Shop</h1>
+    </div>
+  )
+}
 
 const App = () =>  {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Header />}>
+        <Route index element={<Home />}></Route>
+        <Route path='/shop' element={<Shop />}></Route>
+      </Route>
     </Routes>
   );
 };
