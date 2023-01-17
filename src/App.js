383fc710436192131494ba82c8de9ce8b1863ad9
Jack Wuersch <43575552+jackwuersch@users.noreply.github.com>
2023-01-17 19:04:20 +0900

    added router and setup homepage router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import Categories from './components/categories-container/categories-container.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/pages/home/home.component';
 
 const App = () =>  {
-  const categories = [
-    {
-      "id": 1,
-      "title": "BOARDS",
-      "imageUrl": "https://i1.adis.ws/i/ride/ride_2223_feature-banner-sm_thrash-collection-lp?w=408&sm=aspect&aspect=408:302&scaleFit=poi&poi={$this.metadata.pointOfInterest.x},{$this.metadata.pointOfInterest.y},{$this.metadata.pointOfInterest.w},{$this.metadata.pointOfInterest.h}&qlt=75&fmt=webp&fmt.interlaced=true&dpi=96"
-    },
-    {
-      "id": 2,
-      "title": "BINDINGS",
-      "imageUrl": "https://i1.adis.ws/i/ride/ride_2223_mm-banner_bindings_composite-series?w=342&sm=aspect&aspect=342:229&scaleFit=poi&poi={$this.metadata.pointOfInterest.x},{$this.metadata.pointOfInterest.y},{$this.metadata.pointOfInterest.w},{$this.metadata.pointOfInterest.h}&qlt=75&fmt=webp&fmt.interlaced=true&dpi=96"
-    },
-    {
-      "id": 3,
-      "title": "BOOTS",
-      "imageUrl": "https://i1.adis.ws/i/ride/ride_2223_mm-banner_boots_lasso-pro?w=342&sm=aspect&aspect=342:229&scaleFit=poi&poi={$this.metadata.pointOfInterest.x},{$this.metadata.pointOfInterest.y},{$this.metadata.pointOfInterest.w},{$this.metadata.pointOfInterest.h}&qlt=75&fmt=webp&fmt.interlaced=true&dpi=96"
-    }
-  ]
-  
   return (
-    <Categories categories={categories} />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
