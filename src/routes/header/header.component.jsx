f9aa6f42b79a8153df31713e2c8de6bc57dec68d
jack wuersch <jackwuersch@jacks-MacBook-Pro.local>
2023-03-16 17:13:33 +0900

    sign in page

--- src/routes/header/header.component.jsx
+++ src/routes/header/header.component.jsx
@@ -1,47 +1,39 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import './header.component.styles.scss'
 
-import { UserContext } from '../../contexts/user.context'
+import { UserContext } from '../../contexts/user.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
+import './header.styles.scss';
 
-const Header = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
 
-    const signOutHandler = async () => {
-      await signOutUser();
-      setCurrentUser(null);
-    };
+  return (
+    <Fragment>
+      <div className='header'>
+        <Link className='logo-container' to='/'>
+        </Link>
+        <div className='header-links-container'>
+          <Link className='header-link' to='/shop'>
+            SHOP
+          </Link>
 
-    
-    return (
-      <Fragment>
-        <div className='header'>
-            <Link className='logo-container' to='/'>
-                <div>Logo</div>
+          {currentUser ? (
+            <span className='header-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className='header-link' to='/auth'>
+              SIGN IN
             </Link>
-            <div className='header-links-container'>
-                <Link className='header-link' to='/shop'>
-                    SHOP
-                </Link>
-            </div>
-            {
-                currentUser ? (
-                    <span className='header-link' onClick={signOutHandler}>SIGN OUT</span>
-                ) : (
-                    <div className='header-links-container'>
-                        <Link className='header-link' to='/auth'>
-                            Sign In
-                        </Link>
-                    </div>
-                )
-            }
+          )}
         </div>
-        <Outlet />
-      </Fragment>
-    )
-  }
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
-export default Header;
+export default Navigation;
