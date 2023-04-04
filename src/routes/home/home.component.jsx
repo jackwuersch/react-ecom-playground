f891b82ba6440aeb3752fd14155292d33f5c9719
jack wuersch <jackwuersch@jacks-MacBook-Pro.local>
2023-04-04 16:20:47 +0900

    added styled components

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,4 @@
-import Categories from "../../components/categories-container/categories-container.component";
+import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
   const categories = [
@@ -22,7 +22,7 @@ const Home = () => {
     },
   ];
 
-  return <Categories categories={categories} />;
+  return <Directory categories={categories} />;
 };
 
 export default Home;
